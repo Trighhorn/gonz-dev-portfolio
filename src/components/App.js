20c5dffc6364fe5a82417a4d0e908dd068f3165b
trighhorn <trighhorn@gmail.com>
2021-08-01 17:41:36 -0600

    cleaned up filetree and started a homepage component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
-import './App.scss';
+import '../style/App.scss';
+import Homepage from './pages/homepage'
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <Homepage />
     </div>
   );
 }
---
+++ src/components/pages/homepage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Homepage() {
+    return(
+        <div className="homepage">
+            <div className="profilepic">adsfdsa</div>
+            <div className="bio">
+            Hello! I'm Abrahan and I'm a FullStack Software Developer.
+            I'm looking for opportunities in the MERN web development stack Have a look around
+            </div>
+            <div className="links">
+                <div className="link">Github</div>
+                <div className="link">Linkedin</div>
+                <div className="link">Resume</div>
+            </div>
+        </div>
+    )
+}
+
+export default Homepage
