3cca0d31ccc8563d223365080f1c89467f51bb71
trighhorn <trighhorn@gmail.com>
2021-08-02 11:18:52 -0600

    added placeholders and styling for the homepage. set up font awesome.

--- src/components/pages/homepage.js
+++ src/components/pages/homepage.js
@@ -1,17 +1,21 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+
 
 function Homepage() {
     return(
         <div className="homepage">
-            <div className="profilepic">adsfdsa</div>
+            <img className="avatar" alt="Abrahan Gonzalez" src="https://soe.ukzn.ac.za/wp-content/uploads/2018/04/profile-placeholder.png"/>
             <div className="bio">
             Hello! I'm Abrahan and I'm a FullStack Software Developer.
             I'm looking for opportunities in the MERN web development stack Have a look around
             </div>
-            <div className="links">
-                <div className="link">Github</div>
-                <div className="link">Linkedin</div>
-                <div className="link">Resume</div>
+            <div className="links-wrapper">
+                <a href="https://github.com/Trighhorn" className="link github"><FontAwesomeIcon icon={faGithub} size='8x' alt='github'/></a>
+                <a href="https://www.linkedin.com/in/agonzalez-19/" className="link linkedin"><FontAwesomeIcon icon={faLinkedin} size='8x' alt='linkedin'/></a>
+                <a href="https://github.com/Trighhorn" className="link resume"><FontAwesomeIcon icon={faGithub} size='8x'/></a>
             </div>
         </div>
     )
