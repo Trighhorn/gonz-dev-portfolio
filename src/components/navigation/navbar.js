ed74022b03f8e2d82275c18dd2846b8cb61738d9
trighhorn <trighhorn@gmail.com>
2021-08-03 12:36:54 -0600

    fleshed out the contact page with my info and added all of its styling

--- src/components/navigation/navbar.js
+++ src/components/navigation/navbar.js
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 function Navbar() {
   return (
     <div className="navbarWrapper">
-      <div className="left-side"> AG </div>
+      <div className="left-side"> ABRAHAN GONZALEZ </div>
       <nav className="right-side">
         <NavLink exact to="/" activeClassName="navLinkActive">Home</NavLink>
         <NavLink to="/contact" activeClassName="navLinkActive">Contact</NavLink>
--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,10 +1,49 @@
 import React from "react";
+import {
+  faEnvelope,
+  faPhone,
+  faMapMarkedAlt,
+} from "@fortawesome/free-solid-svg-icons";
+import contactPagePicture from "../../images/login.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Contact() {
   return (
-    <div className="contactPage pageWrapper">
-      <div className="contactImage"></div>
-      <div className="contact-info"></div>
+    <div className="contact-page pageWrapper">
+      <div
+        className="contact-image"
+        style={{
+          background: "url(" + contactPagePicture + ") no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+      <div className="contact-info">
+        <div className="contact-bullet-point">
+          <div className="bullet-point">
+            <div className="font-awesome-icon">
+              <FontAwesomeIcon icon={faPhone} />
+            </div>
+            <div className="text">307-677-5546</div>
+          </div>
+          <div className="bullet-point">
+            <div className="font-awesome-icon">
+              <FontAwesomeIcon
+                icon={faMapMarkedAlt}
+              />
+            </div>
+            <div className="text">abgonzdev@gmail.com</div>
+          </div>
+          <div className="bullet-point">
+            <div className="font-awesome-icon">
+              <FontAwesomeIcon
+                icon={faEnvelope}
+              />
+            </div>
+            <div className="text">Salt Lake City, UT</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
