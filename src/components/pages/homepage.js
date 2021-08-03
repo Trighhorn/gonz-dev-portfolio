e36875c1718c40dad56b200b12adba2d8e17611b
trighhorn <trighhorn@gmail.com>
2021-08-03 13:32:22 -0600

    tweaked styling to be more responsive

--- src/components/pages/homepage.js
+++ src/components/pages/homepage.js
@@ -10,13 +10,13 @@ function Homepage() {
         <div className="homepage pageWrapper">
             <img className="avatar" alt="Abrahan Gonzalez" src="https://soe.ukzn.ac.za/wp-content/uploads/2018/04/profile-placeholder.png"/>
             <div className="bio">
-            Hello! I'm Abrahan and I'm a FullStack Software Developer.
+            Hello! I'm Abrahan and I'm a Full Stack Software Developer.<br />
             I'm looking for opportunities in the MERN web development stack Have a look around
             </div>
             <div className="links-wrapper">
-                <a href="https://github.com/Trighhorn" className="link github"><FontAwesomeIcon icon={faGithub} size='8x' alt='github'/></a>
-                <a href="https://www.linkedin.com/in/agonzalez-19/" className="link linkedin"><FontAwesomeIcon icon={faLinkedin} size='8x' alt='linkedin'/></a>
-                <a href="https://drive.google.com/file/d/1FwxHPkf8tlrvWDtTEeHvIVMbcj_ZgkpF/view?usp=sharing" className="link resume"><FontAwesomeIcon icon={faFile} size='7x' alt='resume'/></a>
+                <a href="https://github.com/Trighhorn" className="link github"><FontAwesomeIcon icon={faGithub} alt='github'/></a>
+                <a href="https://www.linkedin.com/in/agonzalez-19/" className="link linkedin"><FontAwesomeIcon icon={faLinkedin} alt='linkedin'/></a>
+                <a href="https://drive.google.com/file/d/1FwxHPkf8tlrvWDtTEeHvIVMbcj_ZgkpF/view?usp=sharing" className="link resume"><FontAwesomeIcon icon={faFile} alt='resume'/></a>
             </div>
         </div>
     )
